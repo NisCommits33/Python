819404845b17cbc41fd8af4238dfd10237478855
NisCommits33 <Nixstha2@gmail.com>
2025-07-21 16:07:57 +0545

    Test

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+# Variable and types
+
+character_name = "Anish shrestha"
+character_age = 23
+
 print("Hello WOrd TEst")
+print("my name is" + character_name)
+print ("he is " + str(character_age))
+print("you have completed "+ str(character_age))
+first_name,last_name = "nischal", "shrestha"
+print(first_name)
+print("my name is" + first_name)
+print(last_name+ " always like to play football in his "  + str(character_age) + "s")
