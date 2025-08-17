c6ba0c3fc73974a323124724a1570fef2520f4d4
NisCommits33 <Nixstha2@gmail.com>
2025-08-17 19:25:49 +0545

    number guessing game created from if and elif

--- number_guessinggame.py
+++ number_guessinggame.py
@@ -1,15 +1,32 @@
 correct_number = "3"
-print("You have 3 chances to guess the number!")
-guessed_number = input("Choose a number from 1 - 5: ")
-if guessed_number == correct_number:
-    print('correct answer')
-else:
-    print("Wrong answer")
-    guessed_number = input("Choose a number from 1 - 5: ")
+wrong_answer_message = "Wrong answer"
+correct_answer_message = "Correct answer"
+numbers =(1,2,3)
+chances =  f"You have {numbers} chances to guess the number! "
+choose_message = f"Choose a number from 1 - 5: "
+
+print(chances)
+guessed_number = int(input(choose_message))
+if 1 <= int(guessed_number) <= 5:
     if guessed_number == correct_number:
-        print('Correct answer')
+        print(correct_answer_message)
     else:
-        print("Wrong answer")
-        guessed_number = input("Choose a number from 1 - 5: ")
-        if guessed_number == correct_number:
-            print('Correct answer')
+        print(wrong_answer_message)
+        print(chances)
+        guessed_number = input(choose_message)
+        if 1 <= int(guessed_number) <= 5:
+            if guessed_number == correct_number:
+                print(correct_answer_message)
+            else:
+                print(wrong_answer_message)
+                print(chances)
+                guessed_number = input(choose_message)
+                if 1 <= int(guessed_number) <= 5:
+                    if guessed_number == correct_number:
+                        print(correct_answer_message)
+                else:
+                    print(choose_message)
+        else:
+            print(choose_message)
+else:
+    print(choose_message + " Try Again")
