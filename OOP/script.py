209b3a82e4612a1b7688b89d0773a5c1efee86b4
NisCommits33 <Nixstha2@gmail.com>
2025-08-20 16:29:38 +0545

    learn: class and object

--- OOP/script.py
+++ OOP/script.py
@@ -1,15 +1,36 @@
 command = input("Enter your command")
 
 class  Dog:
+    def __init__(self,name,breed,owner):
+        self.name = name
+        self.breed = breed
+        self.owner = owner
     def bark(self):
-        print("Welcome to the Dog!")
+        print(dog1.name + " is barking")
     def sit(self):
-        print("dog is sitting!")
-dog1 = Dog() #creating instance of dog
+        print(dog1.name + " is sitting")
+    def introduce(self):
+        print("Welcome to the dog world" + "I am " + dog2.name + " I am a " + dog2.breed)
+class owner:
+    def __init__(self,name,address,number):
+        self.name = name
+        self.address = address
+        self.number = number
+
+owner1 = owner("nischal" , "jarankhu", " 123123")
+dog1 = Dog("ani","German",owner1) #creating instance of dog
+
+owner2 = owner("anis" , "nalaju", "141414")
+dog2 = Dog("nis","nep", owner2)
 
 
 if command == "sit":
     dog1.sit()
+    print("owner details: "  +   dog1.owner.name )
+
+
 if command == "bark":
     dog1.bark()
+elif command == "intro":
+    dog2.introduce()
 
